1384be28931ec144a138ee3444df3e6fd6c87a5d
yevhen.mediakov <eugene.mediakov@gmail.com>
2023-03-14 17:24:07 +0200

    easy timer with Handler

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,30 +16,50 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
+    private lateinit var textView: TextView
+    private var seconds = 0
+    private var isRunning = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter= CustomRecyclerAdapter(fillList())
+        textView = findViewById(R.id.textViewTime)
+        val startButton  = findViewById<Button >(R.id.buttonStart)
+        val stopButton  = findViewById<Button >(R.id.buttonStop)
+        val resetButton  = findViewById<Button >(R.id.buttonReset)
+        startButton.setOnClickListener {
+            isRunning = true
+        }
 
-        Log.d(TAG, "onCreate")
+        stopButton.setOnClickListener {
+            isRunning = false
+        }
+
+        resetButton.setOnClickListener {
+            isRunning = false
+            seconds = 0
+        }
+        runTimer()
     }
-    private fun fillList(): List<String> {
-        val data = mutableListOf<String>()
-        (1..45).forEach { i -> data.add("$i element") }
-        return data
-    }
-    override fun onStart() {
-        super.onStart()
 
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.d(TAG, "onResume")
+    private fun runTimer(){
+        val handler = Handler(Looper.getMainLooper())
+        handler.post(object :Runnable{
+            override fun run() {
+                Log.d(TAG,"timer start")
+                val hours : Int = seconds/3600
+                val minutes: Int = seconds% 3600 /60
+                val sec : Int = seconds % 60
+                val time = String.format(
+                    "%d:%02d:%02d",
+                    hours,minutes,sec
+                )
+                textView.text = time
+                if (isRunning){
+                    seconds++
+                }
+                handler.postDelayed(this,1000)
+            }
+        })
     }
 }
 
