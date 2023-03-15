622e57a9b90027be718c55327668f63b9b7a80bd
yevhen.mediakov <eugene.mediakov@gmail.com>
2023-03-15 14:56:53 +0200

    soccer field canvas drawing

---
+++ app/src/main/java/com/example/fragments/CustomView.kt
@@ -0,0 +1,45 @@
+package com.example.fragments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+
+class CustomView(context: Context?) : View(context) {
+
+    private val markingPaint = Paint()
+
+    init {
+        markingPaint.color = Color.WHITE // цвет разметки
+        markingPaint.style = Paint.Style.STROKE // обводка вместо сплошной заливки
+        markingPaint.strokeWidth = 10.0f // толщина окантовки
+    }
+    private val margin = 15F
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.GREEN)
+        canvas.drawRect(
+            left + margin, top + margin,
+            right - margin, bottom - margin,
+            markingPaint
+        )
+        canvas.drawCircle(this.width/2f,this.height/2f, 250.0F, markingPaint )
+        canvas.drawRect(
+            left + this.height/2f - 55, top + margin,
+            right -  this.height/2f + 55, bottom - this.height + 150f,
+            markingPaint
+        )
+        canvas.drawRect(
+            left + this.height/2f - 55, top + this.height - 150f,
+            right -  this.height/2f + 55, bottom - margin,
+            markingPaint
+        )
+        canvas.drawRect(
+            left + margin, top + this.height/2f,
+            right - margin , bottom - this.height/2f,
+            markingPaint
+        )
+    }
+}
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,40 +2,18 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val TAG = "MainActivity"
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter= CustomRecyclerAdapter(fillList())
+//        setContentView(R.layout.activity_main)
+        val customView = CustomView(this)
+        setContentView(customView)
 
-        Log.d(TAG, "onCreate")
-    }
-    private fun fillList(): List<String> {
-        val data = mutableListOf<String>()
-        (1..45).forEach { i -> data.add("$i element") }
-        return data
-    }
-    override fun onStart() {
-        super.onStart()
-
-        Log.d(TAG, "onStart")
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        Log.d(TAG, "onResume")
-    }
 }
 
