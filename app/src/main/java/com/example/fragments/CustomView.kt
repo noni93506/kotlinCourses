0e3b3b2d4a52558fcd941afa991363f7404417ca
yevhen.mediakov <yevhen.mediakov@stfalcon.com>
2023-03-23 12:14:58 +0200

    fixed basic types

--- app/src/main/java/com/example/fragments/CustomView.kt
+++ app/src/main/java/com/example/fragments/CustomView.kt
@@ -6,39 +6,45 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.view.View
 
-class CustomView(context: Context?) : View(context) {
+class CustomView(context: Context) : View(context) {
 
     private val markingPaint = Paint()
+    companion object{
+        private const val rectangleLineMargin = 15F
+        private const val whiteColor = Color.WHITE
+        private  const val LineWidth = 10.0f
+        private const val circleRadius = 250.0f
 
-    init {
-        markingPaint.color = Color.WHITE
-        markingPaint.style = Paint.Style.STROKE
-        markingPaint.strokeWidth = 10.0f
     }
-    private val margin = 15F
+    init {
+        markingPaint.color = whiteColor
+        markingPaint.style = Paint.Style.STROKE
+        markingPaint.strokeWidth = LineWidth
+    }
+
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawColor(Color.GREEN)
         canvas.drawRect(
-            left + margin, top + margin,
-            right - margin, bottom - margin,
+            left + rectangleLineMargin, top + rectangleLineMargin,
+            right - rectangleLineMargin, bottom - rectangleLineMargin,
             markingPaint
         )
-        canvas.drawCircle(this.width/2f,this.height/2f, 250.0F, markingPaint )
+        canvas.drawCircle(this.width/2f,this.height/2f, circleRadius, markingPaint )
         canvas.drawRect(
-            left + this.height/2f - 55, top + margin,
+            left + this.height/2f - 55, top + rectangleLineMargin,
             right -  this.height/2f + 55, bottom - this.height + 150f,
             markingPaint
         )
         canvas.drawRect(
             left + this.height/2f - 55, top + this.height - 150f,
-            right -  this.height/2f + 55, bottom - margin,
+            right -  this.height/2f + 55, bottom - rectangleLineMargin,
             markingPaint
         )
         canvas.drawRect(
-            left + margin, top + this.height/2f,
-            right - margin , bottom - this.height/2f,
+            left + rectangleLineMargin, top + this.height/2f,
+            right - rectangleLineMargin , bottom - this.height/2f,
             markingPaint
         )
     }
