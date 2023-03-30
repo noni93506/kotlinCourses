305664e6864b10ca0f10dda3c65a81643999ca93
yevhen.mediakov <yevhen.mediakov@stfalcon.com>
2023-03-30 12:56:24 +0300

    simple toast network changes listener

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,41 +1,43 @@
 package com.example.fragments
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.Network
 import android.os.Bundle
 import android.util.Log
-
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val TAG = "MainActivity"
-    }
+    lateinit var textView: TextView
+    lateinit var status: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter= CustomRecyclerAdapter(fillList())
+        status = ""
+        textView = findViewById(R.id.mainTextView)
 
-        Log.d(TAG, "onCreate")
-    }
-    private fun fillList(): List<String> {
-        val data = mutableListOf<String>()
-        (1..45).forEach { i -> data.add("$i element") }
-        return data
-    }
-    override fun onStart() {
-        super.onStart()
 
-        Log.d(TAG, "onStart")
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+
+                Log.d("Network str", "network alive")
+                Toast.makeText(this@MainActivity, "network on", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+            override fun onLost(network: Network) {
+                Log.d("Network str", "network lost")
+                Toast.makeText(this@MainActivity, "network off", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
-    override fun onResume() {
-        super.onResume()
 
-        Log.d(TAG, "onResume")
-    }
+
+
 }
-
